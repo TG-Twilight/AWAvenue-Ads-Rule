395bf7ffc99849bb8a633e79a2c77f88ca88734d
kiss2u <joe.lamplight@gmail.com>
2024-03-20 18:55:34 +0800

    Update RouterOS.py (#52)
    In the build function, changed the output file extension to ".src".

--- script/RouterOS.py
+++ script/RouterOS.py
@@ -1,14 +1,25 @@
 import json
 
 def format_domain(domain_file):
-    domain = []
-    with open(domain_file, 'r') as file:
-        lines = file.readlines()
+  domain = []
+  with open(domain_file, 'r') as file:
+    lines = file.readlines()
 
-    for line in lines:
-        domain_lines = f"ip dns static add address=240.0.0.1 name={line.strip()}"
-        domain.append(domain_lines)
-    return domain
+  for line in lines:
+    domain_line = f"ip dns static add address=240.0.0.1 name={line.strip()}"
+    domain.append(domain_line)
+
+  return domain
 
 def build(domain_file, regex_file):
-    return format_domain(domain_file)
+  return  format_domain(domain_file)
+
+if __name__ == '__main__':
+  domain_file = 'rule/domain.txt'
+  regex_file = 'rule/domain_regex.txt'
+  mikrotik_ros= build(domain_file, regex_file)
+
+  with open('mikrotik_ros.rsc', 'w') as file:
+    file.write('\n'.join(mikrotik_ros))
+
+  print('生成 mikrotik_ros.rsc 文件成功！')
