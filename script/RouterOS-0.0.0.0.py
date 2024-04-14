a932a58b6533b442e9e18010b8fd2e5faad7bd24
Twilight <121682528+TG-Twilight@users.noreply.github.com>
2024-04-14 19:42:26 +0800

    Update RouterOS-0.0.0.0.py

--- script/RouterOS-0.0.0.0.py
+++ script/RouterOS-0.0.0.0.py
@@ -1,7 +1,7 @@
 import json
 
 def format_domain(domain_file):
-    domain = []
+    domain = ["0.0.0.0 local"，"", ""]
     with open(domain_file, 'r') as file:
         lines = file.readlines()
     
