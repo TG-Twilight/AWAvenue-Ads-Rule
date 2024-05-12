d7900beb726cebe32de8013f0b704f1c0e6ff306
heinu123 <heinu333@gmail.com>
2024-05-12 14:22:43 +0800

    添加ipv6规则API&&修复 #71 错误

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ OUT_PATH = os.getcwd() + "/out"
 domain_file=RULE_PATH + "/domain.txt"
 regex_file=RULE_PATH + "/domain_regex.txt"
 ip_file=RULE_PATH + "/ip.txt"
+ip6_file=RULE_PATH + "/ip6.txt"
 
 if not os.path.exists(OUT_PATH):
     print(f"{OUT_PATH} 目录不存在!")
@@ -24,8 +25,10 @@ class RuleList:
             self.regex_list = file.readlines()
         with open(ip_file, 'r') as file:
             self.ip_list = file.readlines()
+        with open(ip6_file, 'r') as file:
+            self.ip6_list = file.readlines()
 
-rule = RuleList(domain_file, regex_file, ip_file)
+rule = RuleList(domain_file, regex_file, ip_file, ip6_file)
 
 def WriteFile(name, text, suffix):
     try:
