ce6862061bc1418612417805c3b01e2599ed1d6b
heinu <118418714+heinu123@users.noreply.github.com>
2024-06-16 23:55:20 +0800

    delete ip

--- script/Clash.py
+++ script/Clash.py
@@ -13,13 +13,7 @@ def format_regex(List):
         regex.append(regex_lines)
     return regex
 
-def format_ip(List):
-    ip = []
-    for line in List:
-        ip_lines = f"  - '{line.strip()}'".replace("$","").replace("^","")
-        ip.append(ip_lines)
-    return ip
 
 def build(rule):
-    clash_list = ["payload:"] + format_ip(rule.ip_list) + format_domain(rule.domain_list) + format_regex(rule.regex_list)
+    clash_list = ["payload:"] + format_domain(rule.domain_list) + format_regex(rule.regex_list)
     return clash_list, ".yaml", "#"
