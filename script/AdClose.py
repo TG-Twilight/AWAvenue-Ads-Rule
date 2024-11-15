c870400f9b1ac0b894577e2ecf05baa99c4fd0c5
Twilight <121682528+TG-Twilight@users.noreply.github.com>
2024-11-15 10:35:05 +0800

    Update AdClose.py

--- script/AdClose.py
+++ script/AdClose.py
@@ -1,18 +1,12 @@
+import json
+
 def format_domain(List):
-    domain = []
+    domain = ["#This rule is only used with AdClose", "", ""]
     for line in List:
-        domain_lines = f"{line.strip()}"
+        domain_lines = f"domain, {line.strip()}"
         domain.append(domain_lines)
     return domain
 
 
-def format_regex(List):
-    regex = []
-    for line in List:
-        regex_lines = f"{line.strip()}"
-        regex.append(regex_lines)
-    return regex
-
 def build(rule):
-    list = format_domain(rule.domain_list) + format_regex(rule.regex_list)
-    return {'list': list, 'suffix': '.txt', 'comment': '!', 'total': len(list)}
+    return {'list': format_domain(rule.domain_list), 'suffix': '.txt', 'comment': '#', 'total': len(rule.domain_list)}
