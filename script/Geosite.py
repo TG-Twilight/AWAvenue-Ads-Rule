b16ed2644d924d4425d1e924fa120c818b947f07
Elysia <118418714+elysias123@users.noreply.github.com>
2025-05-19 10:28:27 +0800

    add Geosite.py

---
+++ script/Geosite.py
@@ -0,0 +1,10 @@
+def format_domain(List):
+    domain = []
+    for line in List:
+        domain_lines = f"full:{line.strip()}"
+        domain.append(domain_lines)
+    return domain
+
+def build(rule):
+    list = format_domain(rule.domain_list)
+    return {'list': list, 'suffix': '.list', 'comment': '#', 'total': len(list)}
