7de072436e94afcfb926900d30faba188bad37fe
Elysia <118418714+elysias123@users.noreply.github.com>
2025-09-08 12:31:37 +0800

    fix #168

--- script/Shadowrocket.py
+++ script/Shadowrocket.py
@@ -1,4 +1,15 @@
 
+def format_comments():
+    now = datetime.now()
+    comments = ["#!name=AWAvenue Ads Rule",
+                f'#!desc=Update time: {now.strftime("%Y-%m-%d %H:%M:%S")} UTC+8\\nHomepage: https://github.com/TG-Twilight/AWAvenue-Ads-Rule',
+                "#!category=广告拦截",
+                "#!author=TG-Twilight[https://github.com/TG-Twilight]",
+                "#!icon=https://avatars.githubusercontent.com/u/121682528?s=144",
+                "#!homepage=https://awavenue.top",
+                "", ""]
+    return comments
+
 def format_domain(List):
     domain = []
     for line in List:
@@ -14,5 +25,5 @@ def format_ip(List):
     return ip
 
 def build(rule):
-    list = format_ip(rule.ip_list) + format_domain(rule.domain_list)
+    list = format_comments() + format_ip(rule.ip_list) + format_domain(rule.domain_list)
     return {'list': list, 'suffix': '.conf', 'comment': '#', 'total': len(list)}
