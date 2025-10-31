98434aa41ed8ebd89fbf7764ab00408402ccf5e0
chuan0712 <G_chuan@outlook.com>
2025-10-31 13:29:39 +0800

    使用full匹配domain,新增domain_suffix和domain_keyword匹配

--- script/Mosdns_v5.py
+++ script/Mosdns_v5.py
@@ -3,10 +3,31 @@ import json
 def format_domain(List):
     domain = []
     for line in List:
-        domain_lines = f"domain:{line.strip()}"
+        domain_lines = f"full:{line.strip()}"
         domain.append(domain_lines)
     return domain
 
+def format_suffix(List):
+    suffix = []
+    for line 在 List:
+        suffix_lines = f"domain:{line.strip()}"
+        suffix.append(suffix_lines)
+    return suffix
+
+def format_keyword(List):
+    keyword = []
+    for line in List:
+        keyword_lines = f"keyword:{line.strip()}"
+        keyword.append(keyword_lines)
+    return keyword
+
+def format_regex(List):
+    regex = []
+    for line in List:
+        regex_lines = f"regexp:{line.strip()}"
+        regex.append(regex_lines)
+    return regex
 
 def build(rule):
-    return {'list': format_domain(rule.domain_list), 'suffix': '.txt', 'comment': '', 'total': len(rule.domain_list)}
+    list = (format_domain(rule.domain_list) + format_suffix(rule.suffix_list) + format_keyword(rule.keyword_list) + format_regex(rule.regex_list))
+    return {'list': list, 'suffix': '.txt', 'comment': '#', 'total': len(list)}
